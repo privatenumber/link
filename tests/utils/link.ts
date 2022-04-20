aac61a50e223f5bf92514e9316a1287d2d05022b
Hiroki Osame <hiroki.osame@gmail.com>
2022-04-20 16:58:38 -0400

    wip

--- tests/utils/link.ts
+++ tests/utils/link.ts
@@ -2,13 +2,7 @@ import path from 'path';
 import fs from 'fs';
 import { execaNode } from 'execa';
 
-console.log('cwd', process.cwd());
-console.log('contents', fs.readdirSync(process.cwd()));
-
 const linkBinPath = path.resolve('./dist/cli.js');
-
-console.log('cwd', linkBinPath);
-console.log('contents', fs.readdirSync(path.dirname(linkBinPath)));
 
 type Options = {
 	cwd: string;
