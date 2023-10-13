ba2dcbbc5a347b4c1bb2b0bae4b74c3b7916f661
Hiroki Osame <hiroki.osame@gmail.com>
2023-10-13 18:24:39 -0400

    Published branch "publish-mode"

--- src/utils/read-json-file.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs/promises';
-
-export const readJsonFile = async (
-	filePath: string,
-) => {
-	const jsonString = await fs.readFile(filePath, 'utf8');
-	return JSON.parse(jsonString) as unknown;
-};
--- tests/fixtures/package-files/non-publish-file.js
+++
@@ -1,1 +0,0 @@
-console.log('non publish file');
