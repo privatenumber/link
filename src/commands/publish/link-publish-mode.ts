288fc82f026560ca0b84387a0e3e485939045791
Hiroki Osame <hiroki.osame@gmail.com>
2024-08-04 17:40:09 +0900

    refactor(publish): hardlink-package util

--- src/commands/publish/link-publish-mode.ts
+++ src/commands/publish/link-publish-mode.ts
@@ -1,13 +1,9 @@
 import path from 'path';
 import fs from 'fs/promises';
 import outdent from 'outdent';
-import {
-	green, magenta, cyan, bold, dim,
-} from 'kolorist';
+import { magenta, bold, dim } from 'kolorist';
 import { readPackageJson } from '../../utils/read-package-json.js';
-import { hardlink } from '../../utils/symlink.js';
-import { cwdPath } from '../../utils/cwd-path.js';
-import { getNpmPacklist } from '../../utils/get-npm-packlist.js';
+import { hardlinkPackage } from './hardlink-package.js';
 
 const isValidSetup = async (
 	linkPath: string,
@@ -69,53 +65,9 @@ export const linkPublishMode = async (
 		return;
 	}
 
-	const [oldPublishFiles, publishFiles] = await Promise.all([
-		getNpmPacklist(
-			linkPathReal,
-
-			/**
-			 * This is evaluated in the context of the new package.json since that
-			 * defines which files belong to the package.
-			 */
-			packageJson,
-		),
-		getNpmPacklist(
-			absoluteLinkPackagePath,
-			packageJson,
-		),
-	]);
-
-	console.log(`Linking ${magenta(packageJson.name)} in publish mode:`);
-	await Promise.all(
-		publishFiles.map(async (file) => {
-			const sourcePath = path.join(absoluteLinkPackagePath, file);
-			const targetPath = path.join(linkPath, file);
-
-			await fs.mkdir(
-				path.dirname(targetPath),
-				{ recursive: true },
-			);
-
-			await hardlink(sourcePath, targetPath);
-
-			const fileIndex = oldPublishFiles.indexOf(file);
-			if (fileIndex > -1) {
-				oldPublishFiles.splice(fileIndex, 1);
-			}
-
-			console.log(
-				`  ${green('✔')}`,
-				cyan(cwdPath(targetPath)),
-				'→',
-				cyan(cwdPath(sourcePath)),
-			);
-		}),
-	);
-
-	await Promise.all(
-		oldPublishFiles.map(async (file) => {
-			const cleanPath = path.join(linkPath, file);
-			await fs.rm(cleanPath);
-		}),
+	await hardlinkPackage(
+		linkPath,
+		absoluteLinkPackagePath,
+		packageJson,
 	);
 };
