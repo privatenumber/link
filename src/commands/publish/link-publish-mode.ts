74fdf1f8f06eed1f23e0e055cde64d1161735bcf
Hiroki Osame <hiroki.osame@gmail.com>
2026-01-11 23:35:42 +0900

    refactor(publish): remove redundant realpath check

--- src/commands/publish/link-publish-mode.ts
+++ src/commands/publish/link-publish-mode.ts
@@ -37,31 +37,20 @@ export const linkPublishMode = async (
 		console.error(
 			outdent`
 			Error: Package ${magenta(packageJson.name)} is not set up
-	
+
 			${bold('Setup instructions')}
 			1. In the Dependency package, create a tarball:
 			  ${dim('$ npm pack')}
-	
+
 			2. In the Consuming package, install the tarball and link the Dependency:
 			  ${dim('$ npm install --no-save <dependency-tarball>')}
 			  ${dim('$ npx link publish <dependency-path>')}
-	
+
 			3. Start developing!
-	
+
 			Learn more: https://npmjs.com/link
 			`,
 		);
-		return;
-	}
-
-	/**
-	 * If it's a symlink, make sure it's in the node_modules directory of the base package.
-	 * e.g. This could happen with pnpm
-	 *
-	 * If it's not, it might be a development directory and we don't want to overwrite it.
-	 */
-	const linkPathReal = await fs.realpath(linkPath);
-	if (!linkPathReal.startsWith(expectedPrefix)) {
 		return;
 	}
 
